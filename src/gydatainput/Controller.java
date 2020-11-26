2936c96c964a7d4c752704cef371c05988786532
Matthew Duff <matthewfduff@gmail.com>
2020-11-26 16:07:52 -0500

    Initial UI Implementation
    + Added main UI
    + UI is resizable and responsive
    - removed sample files

--- src/gydatainput/Controller.java
+++ src/gydatainput/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package gydatainput;
 
 public class Controller {
 
--- src/gydatainput/Main.java
+++ src/gydatainput/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package gydatainput;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Growth & Yield Desktop Application");
+        primaryStage.setScene(new Scene(root, 1200, 800));
         primaryStage.show();
     }
 
