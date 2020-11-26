97f2bddee7f0e822da90c51b343a429dad874806
Matthew Duff <matthewfduff@gmail.com>
2020-11-26 18:35:20 -0500

    Database Functionality
    + Added connection to database
    
    Note that the database connection code will be moved to its own class in the future. The database connection string will also need to be modified by the Growth and Yield database team before connecting.

--- src/gydatainput/Main.java
+++ src/gydatainput/Main.java
@@ -6,6 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main extends Application {
 
     @Override
@@ -14,6 +20,35 @@ public class Main extends Application {
         primaryStage.setTitle("Growth & Yield Desktop Application");
         primaryStage.setScene(new Scene(root, 1200, 800));
         primaryStage.show();
+
+        String connectionString = "jdbc:sqlserver://localhost\\SQLEXPRESS;"
+                + "database=gyPSPPGP;"
+                //+ "user=matthew;" Username and password are only required if integratedSecurity=false
+                //+ "password=;"
+                + "encrypt=false;"
+                + "integratedSecurity=true;" // Automatically uses windows credentials (requires .dll in System32)
+                + "trustServerCertificate=false;"
+                + "loginTimeout=30;";
+
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+        try (Connection connection = DriverManager.getConnection((connectionString))) {
+            System.out.println("Connected to Growth & Yield Database");
+
+            String query = "SELECT TOP (20) * FROM dbo.tblPlot";
+
+            try (Statement statement = connection.createStatement()){
+                ResultSet result = statement.executeQuery(query);
+                while(result.next()) {
+                    System.out.println(result.getString("PlotName"));
+                }
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 
 
